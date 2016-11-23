897dd22506b6ad297229129833b06d1261138597
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-11-24 00:38:32 +0100

    Use correct token when connecting to slack

--- bot.js
+++ bot.js
@@ -7,10 +7,11 @@ var answers = require('./lib/answers');
 var tokens = process.env.token.split(" ");
 _.each(tokens, function(token) {
     var betty = controller.spawn({
-      token: process.env.token
+      token: token
     });
     betty.startRTM(function(err,bot,payload) {
       if (err) {
+        console.log(err);
         throw new Error('Could not connect to Slack');
       }
     });
