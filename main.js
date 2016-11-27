fad604e89153c042befcb3b37ed69c05816d30d2
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-11-27 16:22:07 +0100

    Add missing main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+var pm2 = require('pm2');
+
+var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
+var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
+
+pm2.connect(function() {
+  pm2.start({
+    script    : 'bot.js',
+  }, function(err) {
+    if (err) return console.error('Error while launching applications', err.stack || err);
+    console.log('PM2 and application has been succesfully started');
+    
+    // Display logs in standard output 
+    pm2.launchBus(function(err, bus) {
+      console.log('[PM2] Log streaming started');
+
+      bus.on('log:out', function(packet) {
+       console.log('[App:%s] %s', packet.process.name, packet.data);
+      });
+        
+      bus.on('log:err', function(packet) {
+        console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+      });
+    });
+      
+  });
+});
